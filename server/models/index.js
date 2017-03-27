b1de377b3dae92f1b6afc087de05160bbf8fbfa9
Chiamaka Nwosu <chiamaka.nwosu@andela.com>
2017-03-27 15:49:43 +0100

    updates to fix travisci

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
